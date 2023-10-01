a04efacff8afff85641e18ca3d2e6b666819ccda
LucasVieira <lucasvieirasants@gmail.com>
2023-10-01 17:12:21 -0300

    calculadora de porcentagem

---
+++ src/exercícios/Regrade3.java
@@ -0,0 +1,30 @@
+package exercícios;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Regrade3 {
+    public static void main(String[] args){
+        //variaveis
+        double x, y, valor;
+        //objetos
+        Scanner teclado = new Scanner(System.in);
+        DecimalFormat formatador = new DecimalFormat("#0.0");
+        //Entrada
+        System.out.println("Regra de 3");
+        System.out.println("x% de y = valor");
+        System.out.println("Digite o valor de x: ");
+        x = teclado.nextDouble();
+        System.out.print("Digite o valor de y: ");
+        y = teclado.nextDouble();
+        //processamento
+        valor = (x * y) / 100;
+        System.out.println(x + "% de " + y + " = " +  formatador.format(valor));
+
+
+
+
+    }
+
+
+}
